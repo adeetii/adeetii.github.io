3d76f0813f4dde004e56939267d55bbdd9cc446f
Adeeti Shukla <adeetishukla@Adeetis-MacBook-Air.local>
2022-12-03 18:30:49 +0530

    update

--- javascript/week13.js
+++ javascript/week13.js
@@ -1,33 +1,33 @@
 // object orientation propgramming every thing is an object
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+//<!DOCTYPE html>
+//<html lang="en">
+//<head>
+  //  <meta charset="UTF-8">
+    //<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    //<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    //<title>Document</title>
 
 
 
 
-</head>
-<body>
+//</head>
+//<body>
     
-    <h1>Title of page</h1>
+  //  <h1>Title of page</h1>
     
-    <h3 id="Subtitle"> </h3>
+    //<h3 id="Subtitle"> </h3>
 
-    <p id="myPara"></p>
-    <button onclick= "document.getElementById('myPara').innerHTML='this is the new text in the same P tag'">my Button</button>
+    //<p id="myPara"></p>
+    //<button onclick= "document.getElementById('myPara').innerHTML='this is the new text in the same P tag'">my Button</button>
     
 
-    <script>
-        document.getElementById("Subtitle").innerHTML = "Welcome to my html page with javascript";
-        document.getElementById("myPara"),innerHTML = " hjdkswmenjgvkfdmdncjfvdlsmdj "
-    </script> 
+    //<script>
+      //  document.getElementById("Subtitle").innerHTML = "Welcome to my html page with javascript";
+        //document.getElementById("myPara"),innerHTML = " hjdkswmenjgvkfdmdncjfvdlsmdj "
+    //</script> 
 
-    <h1>INSTAGRAM</h1>
-    <h4>Subsitle</h4>
+    //<h1>INSTAGRAM</h1>
+    //<h4>Subsitle</h4>
 
-</body>
-</html>
+//</body>
+//</html>
